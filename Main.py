c0f9971898b88118aae6d5741cc2767f6e7a0ab5
Muusy <112031065+Ttrmaniac@users.noreply.github.com>
2022-11-12 20:59:28 -0300

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,16 @@
+import math
+print("Created with captain stack")
+num1=float(input("enter the first number :"))
+op = input("sellect the operation :")
+num2 = float(input("enter the second number :"))
+if op== "+" :
+    print(num1+num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "*":
+    print(num1*num2)
+elif op == "/":
+    print(num1 / num2)
+else:
+    print("please enter a real operation ")   
+ #this one is more simple
